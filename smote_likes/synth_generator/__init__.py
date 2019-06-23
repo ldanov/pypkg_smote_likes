6746f5992b372e8f6422970c0ce2a59f0537876a
ldanov <ldanov@users.noreply.github.com>
2019-06-23 18:26:07 +0200

    added restricted SMOTE v2

--- smote_likes/synth_generator/__init__.py
+++ smote_likes/synth_generator/__init__.py
@@ -1,6 +1,9 @@
 from .proto import protoSMOTE
 from .restricted1 import restrictedSMOTE1
+from .restricted2 import restrictedSMOTE2
 
 __all__ = [
-    'protoSMOTE', 'restrictedSMOTE1'
+    'protoSMOTE',
+    'restrictedSMOTE1',
+    'restrictedSMOTE2'
 ]
