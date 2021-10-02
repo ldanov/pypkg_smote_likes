04c0c3c6fc8d3608eee5c58d61368079f7d59d93
ldanov <18732847+ldanov@users.noreply.github.com>
2021-10-02 17:25:40 +0200

    extended nrf testing scope

--- smote_likes/distance_metrics/tests/test_nrf.py
+++ smote_likes/distance_metrics/tests/test_nrf.py
@@ -1,8 +1,7 @@
 from ..nrf import NearestReferenceFeatures
 import numpy
 
-
-def test_NearestReferenceFeatures_bigger():
+def get_nrf_test_data():
     # numpy.random.RandomState(12).randint(0,100,size=(5, 3))
     X_reference = numpy.array([
         [11,  6, 17],
@@ -18,6 +17,10 @@ def test_NearestReferenceFeatures_bigger():
         [1, 11, 18],
         [11,  8,  7]
     ])
+    return X_reference, X_interest
+
+def test_NearestReferenceFeatures_bigger():
+    X_reference, X_interest = get_nrf_test_data()
 
     exp_X_bigger = numpy.array([
         [11, 16, 17],
@@ -31,21 +34,7 @@ def test_NearestReferenceFeatures_bigger():
 
 
 def test_NearestReferenceFeatures_smaller():
-    # numpy.random.RandomState(12).randint(0,100,size=(5, 3))
-    X_reference = numpy.array([
-        [11,  6, 17],
-        [2,  3,  3],
-        [12, 16, 17],
-        [5, 13,  2],
-        [11, 10,  0]
-    ])
-
-    # numpy.random.RandomState(2).randint(0,100,size=(3, 3))
-    X_interest = numpy.array([
-        [8, 15, 13],
-        [1, 11, 18],
-        [11,  8,  7]
-    ])
+    X_reference, X_interest = get_nrf_test_data()
 
     exp_X_smaller = numpy.array([
         [5, 13, 3],
@@ -56,3 +45,23 @@ def test_NearestReferenceFeatures_smaller():
     X_smaller = NearestReferenceFeatures()._closest_nonclass(
         X_interest, X_reference, comparison_type='smaller')
     assert numpy.allclose(X_smaller, exp_X_smaller)
+
+def test_NearestReferenceFeatures_closest_nonclass():
+    X_reference, X_interest = get_nrf_test_data()
+
+    exp_X_bigger = numpy.array([
+        [11, 16, 17],
+        [2, 13, 18],
+        [12, 10, 17]
+    ])
+    exp_X_smaller = numpy.array([
+        [5, 13, 3],
+        [1, 10, 17],
+        [5, 6, 3]
+    ])
+
+    X_bigger, X_smaller = NearestReferenceFeatures().closest_nonclass_values(
+        X_interest, X_reference)
+        
+    assert numpy.allclose(X_smaller, exp_X_smaller)
+    assert numpy.allclose(X_bigger, exp_X_bigger)
